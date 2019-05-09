10b2c33af949565c3e49cf6ad10238b1d4d7aca4
Micaela Bray <micaela.bray@gmail.com>
2019-05-09 17:31:54 -0500

    adds content to left and right panels

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Global variables
+    var songTitles = ["SUCKER", "COOL", "LOVEBUG", "A LITTLE BIT LONGER", "HOLD ON", "FLY WITH ME"]
+
+// Array
+
+// Button
+// function play(){
+//     var audio = document.getElementById("audio");
+//     audio.play();
+// }
