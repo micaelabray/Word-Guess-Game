65bc7e4efa1a4dfa3baff97b516d82fb3b56c4a4
Micaela Bray <micaela.bray@gmail.com>
2019-05-09 17:54:34 -0500

    begins adding JavaScript to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,36 @@
 // Global variables
-    var songTitles = ["SUCKER", "COOL", "LOVEBUG", "A LITTLE BIT LONGER", "HOLD ON", "FLY WITH ME"]
+var songTitles = ["SUCKER", "COOL", "LOVEBUG", "A LITTLE BIT LONGER", "HOLD ON", "FLY WITH ME"];
+var blankSpaces = [];
+var guessedLetters = [];
+var currentGuess;
+var wins = 0;
+var guessesTotal = 10;
+var guessesLeft;
+
+function initialSetup(){ 
+    // Starts the game
+    document.getElementById("directions-text").innerHTML = "Guess the Jonas Brother song title!";
+    // Picks a random songTitle from array
+    currentGuess = songTitles[Math.floor(Math.random()*songTitles.length)];
+    blankSpaces = [];   
+
+    // Gives each letter a value of '_'
+    for(var i = 0; i<currentGuess.length; i++){
+        blankSpaces[i]="_";
+     }
+  
+    // Changes spaces to a blank space instead of an underscore
+    for (var j=0;j<currentGuess.length;j++){
+        if (currentGuess[j] ==" "){
+           blankSpaces[j] = " ";
+        }
+    }
+    // Updates text for number of guesses left
+    guessesLeft = guessesTotal;
+    // Takes away guessed letters
+    guessedLetters = [];
+  }
+
 
 // Array
 
