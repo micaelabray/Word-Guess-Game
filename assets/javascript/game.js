b7035a94a1307b75c512414270411548e86e594e
Micaela Bray <micaela.bray@gmail.com>
2019-05-10 17:30:56 -0500

    Adds functionality to JavaScript title, current word, and guesses left.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,20 @@
 // Global variables
-var songTitles = ["SUCKER", "COOL", "LOVEBUG", "A LITTLE BIT LONGER", "HOLD ON", "FLY WITH ME"];
+var songTitlesArray = ["SUCKER", "COOL", "LOVEBUG", "A LITTLE BIT LONGER", "HOLD ON", "FLY WITH ME"];
 var blankSpaces = [];
 var guessedLetters = [];
-var currentGuess;
 var wins = 0;
 var guessesTotal = 10;
 var guessesLeft;
 
-function initialSetup(){ 
+var currentGuess = songTitlesArray[Math.floor(Math.random()*songTitlesArray.length)];
+    console.log(currentGuess);
+
+function startUp(){ 
     // Starts the game
-    document.getElementById("directions-text").innerHTML = "Guess the Jonas Brother song title!";
-    // Picks a random songTitle from array
-    currentGuess = songTitles[Math.floor(Math.random()*songTitles.length)];
-    blankSpaces = [];   
+    document.getElementById("directionsText").innerHTML = "Guess the Jonas Brothers song title!";
+    document.getElementById("currentGuess").innerHTML = blankSpaces.join(" ");
+    // Picks a random songTitleArray from array
+    blankSpaces = ["_"];
 
     // Gives each letter a value of '_'
     for(var i = 0; i<currentGuess.length; i++){
@@ -25,11 +27,22 @@ function initialSetup(){
            blankSpaces[j] = " ";
         }
     }
+
+    var guessesLeft = document.getElementById("guessesLeft");
+    guessesLeft.innerHTML = guessesTotal;
     // Updates text for number of guesses left
     guessesLeft = guessesTotal;
     // Takes away guessed letters
     guessedLetters = [];
-  }
+
+
+}
+
+document.onkeyup = function(event) {
+    startUp();
+}  
+
+
 
 
 // Array
